fdc70ef830a360290f418f0eb82cc3edce81b21d
anonymous <s1913388@ed.ac.uk>
2022-02-20 18:04:18 +0000

    Done CW1 simple.

--- coursework/sched-mq.cpp
+++ coursework/sched-mq.cpp
@@ -21,7 +21,7 @@ public:
     /**
      * Returns the friendly name of the algorithm, for debugging and selection purposes.
      */
-    const char* name() const override { return "mq"; }
+    const char *name() const override { return "mq"; }
 
     /**
      * Called during scheduler initialisation.
@@ -35,18 +35,20 @@ public:
      * Called when a scheduling entity becomes eligible for running.
      * @param entity
      */
-    void add_to_runqueue(SchedulingEntity& entity) override
+    void add_to_runqueue(SchedulingEntity &entity) override
     {
-        // TODO: Implement me!
+        UniqueIRQLock l;
+        get_runqueue(entity)->enqueue(&entity);
     }
 
     /**
      * Called when a scheduling entity is no longer eligible for running.
      * @param entity
      */
-    void remove_from_runqueue(SchedulingEntity& entity) override
+    void remove_from_runqueue(SchedulingEntity &entity) override
     {
-        // TODO: Implement me!
+        UniqueIRQLock l;
+        get_runqueue(entity)->remove(&entity);
     }
 
     /**
@@ -56,7 +58,45 @@ public:
      */
     SchedulingEntity *pick_next_entity() override
     {
-        // TODO: Implement me!
+        UniqueIRQLock l;
+        // Loop from highest priority (0) to lowest priority (num_levels - 1)
+        for (runqueue_t &runqueue : multilevel_runqueue)
+        {
+            if (!runqueue.empty())
+            {
+                SchedulingEntity *entity = runqueue.dequeue();
+                runqueue.enqueue(entity);
+                return entity;
+            }
+        }
+        // Empty runqueue
+        return nullptr;
+    }
+
+private:
+    typedef SchedulingEntity entity_t;
+    typedef List<entity_t *> runqueue_t;
+
+    // From highest priority (0) to lowest priority (num_levels - 1)
+    static constexpr unsigned int num_levels = 4;
+    runqueue_t multilevel_runqueue[num_levels];
+
+    runqueue_t *get_runqueue(entity_t &entity)
+    {
+        switch (entity.priority())
+        {
+        case SchedulingEntityPriority::REALTIME:
+            return &multilevel_runqueue[0];
+        case SchedulingEntityPriority::INTERACTIVE:
+            return &multilevel_runqueue[1];
+        case SchedulingEntityPriority::NORMAL:
+            return &multilevel_runqueue[2];
+        case SchedulingEntityPriority::DAEMON:
+            return &multilevel_runqueue[3];
+        default:
+            syslog.messagef(LogLevel::FATAL, "Unknown prioity for entity %s.", entity.name().c_str());
+            arch_abort();
+        }
     }
 };
 
